506d0437614377173a1b53f1f4d192fb002c003e
ManimekalaRajendran <manimekala.it13@bitsathy.ac.in>
2016-06-08 10:13:15 +0530

    prime number

---
+++ prime.py
@@ -0,0 +1,10 @@
+# Programs
+for num in range(1,10):
+    if(num==2):
+        print(num)
+    for i in range(2,num):
+        if(num%i==0):
+            break
+        else:
+            print(num)
+            break
